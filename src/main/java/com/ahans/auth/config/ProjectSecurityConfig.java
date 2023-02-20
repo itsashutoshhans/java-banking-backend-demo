2a76331a503d2193f18999557c44dc45e3fd96fe
Ashutosh Hans <ahans@clari.com>
2023-02-20 15:41:53 +0530

    feat: register a new user using register end point and repository

--- src/main/java/com/ahans/auth/config/ProjectSecurityConfig.java
+++ src/main/java/com/ahans/auth/config/ProjectSecurityConfig.java
@@ -3,10 +3,8 @@ package com.ahans.auth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
@@ -15,11 +13,12 @@ import javax.sql.DataSource;
 public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers("/myAccount","/myBalance", "/myLoans", "/myCards").authenticated()
-                .requestMatchers("/contact", "/notices").permitAll();
-        http.formLogin();
-        http.httpBasic();
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+                .requestMatchers("/notices","/contact","/register").permitAll()
+                .and().formLogin()
+                .and().httpBasic();
         return http.build();
     }
 
