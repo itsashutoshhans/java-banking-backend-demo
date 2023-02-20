86f4ffd732e3bfbbe893854f7185915b7381b98b
Ashutosh Hans <ahans@clari.com>
2023-02-20 15:58:22 +0530

    add bCrypt password encoder

--- src/main/java/com/ahans/auth/config/ProjectSecurityConfig.java
+++ src/main/java/com/ahans/auth/config/ProjectSecurityConfig.java
@@ -3,6 +3,7 @@ package com.ahans.auth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,7 +25,7 @@ public class ProjectSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
 }
